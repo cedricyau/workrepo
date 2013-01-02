7bb60357d6314830fa6d2d303dab96e71f154b92
themason <rich_mason_bsc@yahoo.co.uk>
2013-01-02 14:57:39 +0000

    Moved to a more meaningful package

---
+++ BulkStaticUploader/src/musi/interfaces/bsu/pojo/Security.java
@@ -0,0 +1,22 @@
+package musi.interfaces.bsu.pojo;
+
+public class Security {
+	private String id;
+	private String idType;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}	
+	
+	public String getIdType() {
+		return idType;
+	}
+	
+	public void setIdType(String idType) {
+		this.idType = idType;
+	}
+}
