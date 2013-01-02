74519b1bd729ba49f14bbffa00d8f06b01baff75
themason <rich_mason_bsc@yahoo.co.uk>
2013-01-02 15:04:52 +0000

    Moved to new package location

---
+++ BulkStaticUploader/src/musi/interfaces/bsu/utils/XMLUtils.java
@@ -0,0 +1,52 @@
+package musi.interfaces.bsu.utils;
+
+import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
+public class XMLUtils
+{
+	public static String prettyPrint(String xml)
+	{
+		if (xml.isEmpty()) {
+			throw new RuntimeException("xml was null or blank in prettyPrint()");
+		}
+
+		final StringWriter sw;
+		 
+		try
+		{
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			Document document = DocumentHelper.parseText(xml);
+			sw = new StringWriter();
+			final XMLWriter writer = new XMLWriter(sw, format);
+	        writer.write(document);
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Error pretty printing xml:\n" + xml, e);
+		}
+		return sw.toString();
+	}
+
+	public static String removeAttributes(String xml)
+	{
+		Pattern p = Pattern.compile(" type=\"([a-zA-Z]*)\"");
+		Matcher m = p.matcher(xml);
+		StringBuffer sb = new StringBuffer(xml.length());
+
+		while (m.find()) {
+			m.appendReplacement(sb, "");
+		}
+		m.appendTail(sb);
+
+		return sb.toString();
+	}
+	
+	public static String xmlEncode(String value) {
+		return value.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&quot;");
+	}
+}
