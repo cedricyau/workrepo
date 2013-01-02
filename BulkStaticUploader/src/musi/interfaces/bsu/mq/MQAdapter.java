07ad4388f5d241544be2608af10064e3ed87eb6e
themason <rich_mason_bsc@yahoo.co.uk>
2013-01-02 14:58:44 +0000

    Moved to new package location

---
+++ BulkStaticUploader/src/musi/interfaces/bsu/mq/MQAdapter.java
@@ -0,0 +1,83 @@
+package musi.interfaces.bsu.mq;
+
+import java.io.IOException;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import org.apache.log4j.Logger;
+
+import musi.interfaces.bsu.jms.MessageService;
+
+public class MQAdapter {
+	String destinationQueue;
+	String replyToQueue;
+	String sourceSystem;
+	
+	MessageService messageService;
+
+	Logger log = Logger.getLogger(getClass());
+	
+	public void writeMessage(String message) throws IOException {					
+		messageService.sendMessage(message, destinationQueue, sourceSystem, replyToQueue, true);
+	}
+	
+	public String readMessage() throws IOException {	
+		String messageBody = null;		
+		Message message;
+		
+		try {
+			message = messageService.readMessage(replyToQueue);
+
+			if (message instanceof TextMessage) {
+				TextMessage textMessage = (TextMessage) message;
+				messageBody = textMessage.getText();
+				if (messageBody.isEmpty() || messageBody == null) {
+					log.warn("No message available");
+					return null;
+				}
+			}
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		return messageBody;
+	}
+	
+	public String readMessage(long timeout) throws IOException {	
+		String messageBody = null;		
+		Message message;
+		
+		try {
+			message = messageService.readMessage(replyToQueue, Long.valueOf(timeout));
+			
+			if (message instanceof TextMessage) {
+				TextMessage textMessage = (TextMessage) message;
+				messageBody = textMessage.getText();
+				if (messageBody.isEmpty() || messageBody == null) {
+					log.warn("No message available");
+					return null;
+				}
+			}	
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		return messageBody;
+	}
+
+	public void setSourceSystem(String sourceSystem) {
+		this.sourceSystem = sourceSystem;
+	}
+
+	public void setDestinationQueue(String destinationQueue) {
+		this.destinationQueue = destinationQueue;
+	}
+
+	public void setReplyToQueue(String replyToQueue) {
+		this.replyToQueue = replyToQueue;
+	}
+	
+	public void setMessageService(MessageService messageService) {
+		this.messageService = messageService;
+	}
+}
