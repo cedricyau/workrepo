03d82bcc2ee8552c51ff6f1a4472b3eef7be76e3
themason <richard@ghmason.uk>
2016-04-15 14:55:28 +0100

    added author refactored tests

--- BinaryAddition/src/test/java/my/coding/challenge/test/BinaryAdditionTest.java
+++ BinaryAddition/src/test/java/my/coding/challenge/test/BinaryAdditionTest.java
@@ -6,48 +6,68 @@ import my.coding.challenge.BinaryAddition;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Richard Mason
+ * Test class to unit test BinaryAddition.java 
+ *
+ */
 public class BinaryAdditionTest {
 	BinaryAddition ba = new BinaryAddition();
 	
 	@Test
 	public void testValidInput1() {
-		assertEquals("Testing sum of 2 small binary numbers", "100", ba.add("10", "10"));
+		assertEquals("Pass expected", "100", ba.add("10", "10"));
 	}
 	
 	@Test
 	public void testValidInput2() {
-		assertEquals("1000100110", ba.add("111110100", "110010"));
+		assertEquals("Pass expected", "1000100110", ba.add("111110100", "110010"));
 	}
 	
 	@Test
 	public void testValidInput3() {
-		assertEquals("110100111110110101111000110", 
+		assertEquals("Pass expected", 
+				"110100111110110101111000110", 
 				ba.add("101111101011110000011111111", 
 				"101010011000101011000111"));
 	}
 	
 	@Test
 	public void testValidInput4() {
-		assertEquals("111001011001011010110111101100001100011001000011110001110001100110000010110010011100111111001101101100010100101011110110100000000000000000000001", 
+		assertEquals("Pass expected",
+				"111001011001011010110111101100001100011001000011110001110001100110000010110010011100111111001101101100010100101011110110100000000000000000000001", 
 				ba.add("11100101100101101011011110110000110001100100001111000111000110010110110110011100110011010000010111010000000000000000000000000000000000000000000", 
 				"11100101100101101011011110110000110001100100001111000111000110011001011111110110110100101001010110010010100101011110110100000000000000000000001"));
 	}
 	
 	@Test
-	public void testInvalidResult1() {
-		String result = ba.add("10", "11"); // expect result of 101
-		assertEquals("101", result);
-		assertNotEquals("100", result);
+	public void testIncorrectResult1() {
+		assertEquals("Failure expected", "100", ba.add("10", "11"));
 	}
 	
 	@Test
-	public void testInvalidArguments() {
-		
+	public void testIncorrectResult2() {
+		assertNotEquals("Failure expected", "101", ba.add("11", "10"));
 	}
 	
+	@Test(expected = IllegalArgumentException.class) 
+	public void testInvalidArguments1() {
+		ba.add("12345", "11"); // Pass expected
+	}
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void testInvalidArguments2() {
+		ba.add("11", "12345"); // Pass expected
+	}
+	
+	// main created to assist proving solution
 	public static void main(String[] args) {
-		if (args.length == 2) { 
-			new BinaryAddition().add(args[0], args[1]);
+		if (args.length == 3) {
+			BinaryAddition ba = new BinaryAddition();
+			String result = ba.add(args[0], args[1]);
+			System.out.println(result);
+			System.out.println(args[2].equals(result));
 		} else {
 			System.out.println("Terminating...received "+ args.length + " argument(s) when expecting 2.");
 		}
